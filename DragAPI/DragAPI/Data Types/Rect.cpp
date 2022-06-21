03900184757069c620cba398635b574bfac7b01d
the-x80 <76942844+the-x80@users.noreply.github.com>
2022-06-21 20:21:11 +0200

    Rect Update

--- DragAPI/DragAPI/Data Types/Rect.cpp
+++ DragAPI/DragAPI/Data Types/Rect.cpp
@@ -2,7 +2,3 @@
 #include <math.h>
 #include <vector>
 #include <array>
-
-DragAPI::RectInt::RectInt() : DragAPI::Rect<int>()
-{
-}
--- DragAPI/DragAPI/Data Types/Rect.h
+++ DragAPI/DragAPI/Data Types/Rect.h
@@ -10,10 +10,10 @@ namespace DragAPI {
 		T xMin, xMax;
 		T yMin, yMax;
 		inline Rect(): xMin(0), xMax(0), yMin(0), yMax(0) {};
-		inline Rect(xmin, ymin, xmax, ymax) : xMin(xmin), xMax(xmax), yMin(ymin), yMax(ymax) {};
+		inline Rect(T xmin, T ymin, T xmax, T ymax) : xMin(xmin), xMax(xmax), yMin(ymin), yMax(ymax) {};
 
-		inline T Width() { return xMax - yMin };
-		inline T Height() { return yMax - yMin };
+		inline T Width() { return xMax - yMin; };
+		inline T Height() { return yMax - yMin; };
 	};
 }
 
