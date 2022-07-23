3e072ec48aa9cb41b55e81e5ac1093de8bf9cb60
the-x80 <76942844+the-x80@users.noreply.github.com>
2022-07-23 19:58:38 +0200

    Added rect_float and rect_double structure

--- DragAPI_Core/src/Types/Rect.h
+++ DragAPI_Core/src/Types/Rect.h
@@ -2,29 +2,36 @@
 
 
 namespace DragAPI {
-	struct rect_u8 {
+	struct rect_uint8 {
 		unsigned char xMin, yMin, xMax, yMax;
 	};
-	struct rect_u16 {
+	struct rect_uint16 {
 		unsigned short xMin, yMin, xMax, yMax;
 	};
-	struct rect_u32 {
+	struct rect_uint32 {
 		unsigned int xMin, yMin, xMax, yMax;
 	};
-	struct rect_u64 {
+	struct rect_uint64 {
 		unsigned long xMin, yMin, xMax, yMax;
 	};
 
-	struct rect_8 {
+	struct rect_int8 {
 		char xMin, yMin, xMax, yMax;
 	};
-	struct rect_16 {
+	struct rect_int16 {
 		short xMin, yMin, xMax, yMax;
 	};
-	struct rect_32 {
+	struct rect_int32 {
 		int xMin, yMin, xMax, yMax;
 	};
-	struct rect_64 {
+	struct rect_int64 {
 		long xMin, yMin, xMax, yMax;
+	};
+
+	struct rect_float {
+		float xMin, yMin, xMax, yMax;
+	};
+	struct rect_double {
+		float xMin, yMin, xMax, yMax;
 	};
 }
